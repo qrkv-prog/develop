f117cb1c5e3c7e270c8e4acaa1ef3ff5189c6e8a
unknown <ravikumarverma686@gmail.com>
2019-12-08 22:02:52 +0530

    first commit

--- src/com/company/ReaderThread.java
+++ src/com/company/ReaderThread.java
@@ -1,4 +1,79 @@
 package com.company;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class ReaderThread {
+public class ReaderThread
+{
+    String file1;
+    String file2;
+    public ReaderThread(String file1,String file2)
+    {
+        this.file1 = file1;
+        this.file2 = file2;
+    }
+    public  long getLineCount(File file) throws IOException {
+
+        try (Stream<String> lines = Files.lines(file.toPath())) {
+            return lines.count();
+        }
+    }
+    public void FileReaderThread() throws IOException, InterruptedException {
+        BufferedReader reader1;
+        BufferedReader reader2;
+        // assumption both the files have same number of lines
+        int count = (int) getLineCount(new File(file1));
+        String[] worker1 = new String[101];
+        String[] worker2 = new String[101];
+        List<Thread> pool = new ArrayList<Thread>();
+        String result  = "";
+        try {
+            int j  =0;
+            // reading the 1st json file
+            reader1 = new BufferedReader(new FileReader(file1));
+            reader2 = new BufferedReader(new FileReader(file2));
+            int threadCounter =1;
+            String line1 = reader1.readLine();
+            String line2 = reader2.readLine();
+            --count;
+            while (line1 != null && line2 != null) {
+
+                worker1[threadCounter] = new String(line1);
+                worker2[threadCounter] = new String(line2);
+                line1 = reader1.readLine();
+                // reading the 2nd json file
+                line2 = reader2.readLine();
+                if(threadCounter % 100 == 0 || count == 0)
+                {
+                    URLConnectionReader url = new URLConnectionReader(worker1,worker2);
+                    Thread t  = new Thread(url);
+                    pool.add(t);
+                    t.start();
+                    ++j;
+                }
+                --count;
+                ++threadCounter;
+
+            }
+            reader1.close();
+            reader2.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        for(int i =0;i<pool.size();++i)
+        {
+            pool.get(i).join();
+
+        }
+
+    }
+
 }
--- src/com/company/URLConnectionReader.java
+++ src/com/company/URLConnectionReader.java
@@ -1,4 +1,82 @@
 package com.company;
+import java.net.*;
+import java.io.*;
 
-public class URLConnectionReader {
+public class URLConnectionReader implements  Runnable  {
+
+    String[] args1;
+    String[] args2;
+
+    public URLConnectionReader(String[] w1,String[] w2)
+    {
+        args1 = w1;
+        args2 = w2;
+    }
+    // this function reads the response and compares them
+    public void ReadURL() throws Exception {
+        for(int i =1;i<=args1.length ;++i)
+        {
+            if(args1[i] == null || args2[i] == null) break;
+            System.out.println(args1[i] + "," + args2[i]);
+            URL url1 = new URL(args1[i]);
+            URL url2  = new URL(args2[i]);
+            HttpURLConnection yc1 = (HttpURLConnection)url1.openConnection();
+            yc1.addRequestProperty("User-Agent", "Mozilla/4.0");
+            yc1.setRequestMethod("GET");
+            yc1.setRequestProperty("Content-Type", "application/json");
+            HttpURLConnection yc2 = (HttpURLConnection)url2.openConnection();
+            yc2.setRequestMethod("GET");
+            yc2.setRequestProperty("Content-Type", "application/json");
+            yc2.addRequestProperty("User-Agent", "Mozilla/4.0");
+            BufferedReader in1 = null;
+            BufferedReader in2 = null;
+            try  {
+                in1 = new BufferedReader(
+                        new InputStreamReader(
+                                yc1.getInputStream()));
+                in2 = new BufferedReader(
+                        new InputStreamReader(
+                                yc2.getInputStream()));
+                String inputLine1;
+                String inputLine2;
+                String result = "";
+                while ((inputLine1 = in1.readLine()) != null && (inputLine2 = in2.readLine()) != null) {
+                    inputLine1 = inputLine1.trim();
+                    inputLine2 = inputLine2.trim();
+                    if(inputLine1.compareToIgnoreCase(inputLine2) != 0)
+                    {
+                        result = "not equals";
+                        break;
+                    }
+                    System.out.println("1= " + inputLine1 + "," + "2=" + inputLine2);
+                }
+                in1.close();
+                in2.close();
+
+                if(result.length() == 0)
+                    result = "equals";
+                System.out.println(result);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally {
+                in1.close();
+                in2.close();
+
+            }
+
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            ReadURL();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
